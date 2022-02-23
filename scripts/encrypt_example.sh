76ecae0e6eec8cf2875b00f89bcdd19b22d33fbe
Łukasz Gryglicki <lgryglicki@cncf.io>
2022-02-23 09:39:10 +0000

    Disable debug mode
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- scripts/encrypt_example.sh
+++ scripts/encrypt_example.sh
@@ -10,5 +10,5 @@ export JIRA_NO_INCREMENTAL=1
 # curl -s -XPOST -H 'Content-Type: application/json' "${ESURL}/last-update-cache/_delete_by_query" -d'{"query":{"term":{"key.keyword":"Jira:https://jira.riscv.org"}}}' | jq -rS '.' || exit 1
 ../insights-datasource-github/encrypt "`cat ./secrets/user.secret`" > ./secrets/user.encrypted.secret || exit 2
 ../insights-datasource-github/encrypt "`cat ./secrets/token.secret`" > ./secrets/token.encrypted.secret || exit 3
-./jira --jira-url='https://jira.riscv.org' --jira-debug=1 --jira-es-url="${ESURL}" --jira-user="`cat ./secrets/user.encrypted.secret`" --jira-token="`cat ./secrets/token.encrypted.secret`" --jira-stream="${STREAM}" $* 2>&1 | tee run.log
+./jira --jira-url='https://jira.riscv.org' --jira-debug=0 --jira-es-url="${ESURL}" --jira-user="`cat ./secrets/user.encrypted.secret`" --jira-token="`cat ./secrets/token.encrypted.secret`" --jira-stream="${STREAM}" $* 2>&1 | tee run.log
 #./jira --jira-url='https://jira.acumos.org' --jira-debug=1 --jira-es-url="${ESURL}" --jira-user='' --jira-token='' --jira-stream="${STREAM}" $* 2>&1 | tee run2.log
